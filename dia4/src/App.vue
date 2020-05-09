f8a11b269e99a7cecdc0735bd04a6c266df35a5e
joelmatiassilva <josilva@mailexternos.com.ar>
2020-05-09 01:08:54 -0300

    refactor

--- dia4/src/App.vue
+++ dia4/src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ListadoDeMaterias/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListadoDeMaterias from './components/ListadoDeMaterias.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ListadoDeMaterias
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ dia4/src/components/ListadoDeMaterias.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="">
+    <h1>{{ carrrera.título }}</h1>
+
+
+    <span v-on:click="toogleMostrarMaterias">Mostrar Materias
+        {{ mostrarMaterias ? '🐵' : '🙈' }}
+    </span>
+
+    <ul id="lasMaterias" v-show=mostrarMaterias>
+        <li v-for="materia in carrrera.materias" :key="materia.dia">
+          <div v-if="materia.dificultad > 3" v-bind:style="{ color: hardColor }">{{ materia.dia }} - {{ materia.nombre }}</div>
+          <div v-else v-bind:style="{ color: easyColor }">{{ materia.dia }} - {{ materia.nombre }}</div>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListadoDeMaterias',
+  props: {
+    msg: String
+  },
+    data () {
+    return {
+    regional: "FRBA",
+    carrera: "sistemas",
+    hardColor: 'red',
+    easyColor: 'blue',
+    mostrarMaterias: true,
+      carrrera: {
+        título: 'Materias de Sistemas',
+        img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+        materias: [
+          { dia: 'Lunes', nombre: "Análisis Matemática", dificultad: 5 },
+          { dia: 'Martes', nombre: "Álgebra", dificultad: 4 },
+          { dia: 'Miercoles', nombre: "Matemática Discreta", dificultad: 5 },
+          { dia: 'Jueves', nombre: "Sistemas de Organizaciones", dificultad: 2 },
+          { dia: 'Viernes', nombre: "Química", dificultad: 3 },
+          { dia: 'Sábado', nombre: "Algoritmo y estructura de datos", dificultad: 4 }
+        ],
+      }
+    }
+  },
+    computed : {
+      tituloComputed () {
+        console.log("titulo computado - funciones que siempre devuelve un valor.")
+        return `${this.regional} - ${this.carrera}`
+      }
+  },
+
+  watch: {
+    mostrarMaterias (newVal, oldVal) {
+        console.log(" mostrarMaterias - watch")
+        console.log(newVal, oldVal)
+    }
+  },
+  
+  created () {
+    console.log('Created...')
+  },
+
+  mounted () {
+    console.log('Mounted...')
+  },
+
+  methods: {
+    toogleMostrarMaterias () {
+        console.log("toogleMostrarMaterias")
+        this.mostrarMaterias = !this.mostrarMaterias
+    }
+  }
+
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
